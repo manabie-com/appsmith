e6ec9cf4d391ff4fa8fdd3809c14721451664c3e
thanhloc1993 <loc.nguyen@manabie.com>
2023-08-08 14:42:19 +0700

    remove id

--- app/client/src/widgets/BaseInputWidget/component/index.tsx
+++ app/client/src/widgets/BaseInputWidget/component/index.tsx
@@ -378,7 +378,6 @@ const customIconWrapper = (
       <svg
         style={fillColor ? { fill: fillColor } : {}}
         version="1.1"
-        id="Capa_1"
         xmlns="http://www.w3.org/2000/svg"
         x="0px"
         y="0px"
